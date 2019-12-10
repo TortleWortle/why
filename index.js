01eafc52349f53126572a60ba42602205459bd6b
TortleWortle <bas.kuipers@me.com>
2019-12-10 04:17:11 +0100

    I should learn how to use breakpoints someday, but that day is not today

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const traverse = inp => {
     let out = Object.keys(inp).reduce((obj, key) => {
         if (typeof obj[key] == "object") {
-            // console.log("traversing", key)
             obj[key] = traverse(obj[key])
         }
         if (typeof obj[key] == "function") {
-            // console.log("function", key)
             str = obj[key].toString()
-            // console.log(str)
 
             let matches = [
                 ...str.matchAll(/this\.requires\.([A-Za-z0-9\_\-\/\~]+)/),
                 ...str.matchAll(/this\.requires\[\'([A-Za-z0-9\_\-\/\~]+)\'\]/),
                 ...str.matchAll(/this\.requires\[\"([A-Za-z0-9\_\-\/\~]+)\"\]/)
             ]
-            // console.log("Matches:", matches.length)
             if (matches.length > 0) {
                 obj.requires = {}
             }
